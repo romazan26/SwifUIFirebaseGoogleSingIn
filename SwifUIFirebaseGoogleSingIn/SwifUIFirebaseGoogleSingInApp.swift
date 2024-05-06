a9465a334945225b078f7eaa180b09ce18e0dce4
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-05-06 09:52:59 +0300

    Initial Commit

---
+++ SwifUIFirebaseGoogleSingIn/SwifUIFirebaseGoogleSingInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIFirebaseGoogleSingInApp.swift
+//  SwifUIFirebaseGoogleSingIn
+//
+//  Created by Роман on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIFirebaseGoogleSingInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

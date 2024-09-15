99a2cce3d55b71e99b104bb17f791c5dcf9c9b95
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-09-15 19:46:15 +0300

    good size image in pencel

--- SwifUIFirebaseGoogleSingIn/PhotoEditor/PencilView.swift
+++ SwifUIFirebaseGoogleSingIn/PhotoEditor/PencilView.swift
@@ -14,8 +14,19 @@ struct PencilView: View {
     @State var image: UIImage = .no
     
     var body: some View {
-            PencilKitview(pencilKitCanvasView: $canvasView, imageData: $image)
-                .frame(width: 500, height: 500)
+        ZStack {
+            GeometryReader{ proxy -> AnyView in
+                let size = proxy.frame(in: .global).size
+                
+                return AnyView(
+                    ZStack {
+                        PencilKitview(rect: size, pencilKitCanvasView: $canvasView, imageData: $image)
+                    }
+                )
+            }
+            
+                
+        }
     }
 }
 
@@ -27,6 +38,7 @@ struct PencilKitview: UIViewRepresentable {
     
     let toolPicker = PKToolPicker()
     typealias UIViewType = PKCanvasView
+    var rect: CGSize
     
     @Binding var pencilKitCanvasView: PKCanvasView
     @Binding var imageData: UIImage
@@ -42,17 +54,19 @@ struct PencilKitview: UIViewRepresentable {
         pencilKitCanvasView.isOpaque = false
         pencilKitCanvasView.backgroundColor = .clear
         
+        let imageView = UIImageView(image: imageData)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.frame = CGRect(x: 0, y: 0, width: rect.width, height: rect.height)
+        let subView = pencilKitCanvasView.subviews[0]
+        subView.addSubview(imageView)
+        subView.sendSubviewToBack(imageView)
+        
         return pencilKitCanvasView
     }
     
     func updateUIView(_ uiView: PKCanvasView, context: Context) {
-        let imageView = UIImageView(image: imageData)
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
         
-        let subView = pencilKitCanvasView.subviews[0]
-        subView.addSubview(imageView)
-        subView.sendSubviewToBack(imageView)
     }
     
     
